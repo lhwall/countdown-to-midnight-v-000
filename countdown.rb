d459fd098a33912cc8f6f1733bc8854739e2093d
Lucy Wall <lhwall@gmail.com>
2018-03-19 19:17:13 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,10 @@
 #write your code here
 
 def countdown(input)
-  integer = input.to_i
-  while integer > 0 
-  
+  number = input.to_i
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1
+end
+puts "HAPPY NEW YEAR!"
 end

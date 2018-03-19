98ef3d084d5efa9e511296a0103d49052fd17f31
Lucy Wall <lhwall@gmail.com>
2018-03-19 19:17:26 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(input)
 end
 puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(input)
+  number = input.to_i
+  while number > 0 
+  puts "#{number} SECOND(S)!"
+  number -= 1
+end
+puts "HAPPY NEW YEAR!"
+end
